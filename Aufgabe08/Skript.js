dd626535cce610e9f95e8b7010fd429c444c7872
HannahSchmitt <hannah.gabriele.schmitt@hs-furtwangen.de>
2020-06-20 13:21:26 +0200

    aufgabe08

--- Aufgabe08/Skript.js
+++ Aufgabe08/Skript.js
@@ -1,15 +1,15 @@
 "use strict";
 var Aufgabe08;
 (function (Aufgabe08) {
-    let anmelden = document.getElementById("button");
-    anmelden.addEventListener("click", communicate);
-    async function communicate() {
+    let button = document.getElementById("button");
+    button.addEventListener("click", addUrl);
+    async function addUrl() {
         let formData = new FormData(document.forms[0]);
-        let url = "https://gis-example.herokuapp.com";
+        let url = "https://aufgabe8gis.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
         await fetch(url);
-        for (let entry of formData) {
+        for (let entry of query) {
             console.log(entry);
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
--- Aufgabe08/Skript.ts
+++ Aufgabe08/Skript.ts
@@ -1,14 +1,14 @@
 namespace Aufgabe08 {
-    let anmelden: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
-    anmelden.addEventListener("click", communicate);
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    button.addEventListener("click", addUrl);
 
-    async function communicate(): Promise<void> {
+    async function addUrl(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://gis-example.herokuapp.com";
+        let url: string = "https://aufgabe8gis.herokuapp.com";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
         await fetch(url);
-        for (let entry of formData) {
+        for (let entry of query) {
             console.log(entry);
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
